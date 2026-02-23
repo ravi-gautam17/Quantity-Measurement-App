f3cd7be7a5369224211ca06256083b4ee15f995e
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-02-23 11:25:42 +0530

    [Ravi]: added UC2: Feet and Inches measurement equality program added

--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -1,6 +1,6 @@
 package com.apps.quantitymeasurement;
 
-import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+import com.apps.quantitymeasurement.QuantityMeasurementApp.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -37,5 +37,38 @@ public class QuantityMeasurementAppTest {
         Feet feet1 = new Feet(1.0);
         Object obj = new Object();
         Assertions.assertNotEquals(feet1, obj);
+    }
+    
+    @Test
+    public void twoSameInchesValues() {
+        Inches inches1 = new Inches(1.0);
+        Inches inches2 = new Inches(1.0);
+        Assertions.assertEquals(inches1, inches2);
+    }
+
+    @Test
+    public void twoDifferentInchesValues() {
+    	Inches inches1 = new Inches(1.0);
+    	Inches inches2 = new Inches(2.0);
+        Assertions.assertNotEquals(inches1, inches2);
+    }
+
+    @Test
+    public void inchesValueAndNull() {
+    	Inches inches1 = new Inches(1.0);
+        Assertions.assertNotEquals(null, inches1);
+    }
+
+    @Test
+    public void sameInchesReference() {
+    	Inches inches1 = new Inches(1.0);
+        Assertions.assertEquals(inches1, inches1);
+    }
+
+    @Test
+    public void inchesAndDifferentType() {
+    	Inches inches1 = new Inches(1.0);
+        Object obj = new Object();
+        Assertions.assertNotEquals(inches1, obj);
     }
 }
