02f211d95a779d8829d3d7ffa75ac6dc38abe42f
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-02-24 00:58:28 +0530

    [Ravi]: added UC8: Refactoring Unit Enum to Standalone with Conversion Responsibility

---
+++ src/main/java/com/apps/quantitymeasurement/LengthUnit.java
@@ -0,0 +1,25 @@
+package com.apps.quantitymeasurement;
+
+
+public enum LengthUnit {
+	FEET(1.0),
+	INCHES(1.0/12.0),
+	YARDS(3.0),
+	CENTIMETERS(0.0328084);
+	
+	private final double conversionFactor;
+	
+	LengthUnit(double conversionFactor){
+		this.conversionFactor=conversionFactor;
+	}
+	
+	public double getConversionFactor() {
+		return conversionFactor;
+	}
+	public double convertToBaseUnit(double value) {
+		return value*this.getConversionFactor();
+	}
+	public double convertFromBaseUnit(double value) {
+		return value*LengthUnit.FEET.getConversionFactor()/this.getConversionFactor();
+	}
+}
