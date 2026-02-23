0dfb337ed388638c7292ed4158f706cbf4a88de7
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-02-23 11:24:03 +0530

    [Ravi]: added UC2: Feet and Inches measurement equality testing class

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -17,11 +17,39 @@ public class QuantityMeasurementApp {
 			Feet other = (Feet) obj;
 			return Double.compare(other.value, this.value)==0;
 		}
+		public double getValue() {
+			return this.value;
+		}
+	}
+	
+	public static class Inches{
+		private final double value;
+		
+		public Inches(double value) {
+			this.value = value;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if(this==obj) return true;
+			if(obj==null || getClass() != obj.getClass()) return false;
+			
+			Inches other = (Inches) obj;
+			return Double.compare(other.value, this.value)==0;
+		}
+		public double getValue() {
+			return this.value;
+		}
 	}
 	public static void main(String[] args) {
 		Feet f1 = new Feet(1.0);
 		Feet f2 = new Feet(1.0);
 		System.out.println("Input: 1.0 ft and 1.0 ft");
 		System.out.println("Output: Equals ("+f1.equals(f2)+")");
+		
+		Inches i1 = new Inches(1.0);
+		Inches i2 = new Inches(1.0);
+		System.out.println("Input: 1.0 inches and 1.0 inches");
+		System.out.println("Output: Equals ("+i1.equals(i2)+")");
 	}
 }
