ac64cfbf6a8426306354a75c472783027e1424bf
Ravi Gautam <180750145+ravi-gautam17@users.noreply.github.com>
2026-02-28 17:16:30 +0530

    [Ravi]: added deault method in Quantity Measurement app program added

--- src/main/java/com/apps/quantitymeasurement/IMeasurable.java
+++ src/main/java/com/apps/quantitymeasurement/IMeasurable.java
@@ -1,8 +1,24 @@
 package com.apps.quantitymeasurement;
 
+
+@FunctionalInterface
+interface SupportsArithmetic{
+	boolean isSupported();
+}
+
 public interface IMeasurable {
 	double getConversionFactor();
 	double convertToBaseUnit(double value);
 	double convertFromBaseUnit(double baseValue);
 	String getUnitName();
+	
+	default boolean supportsArithmetic() {
+		return true;
+	}
+	
+	default void validateOperationSupport(String operation) {
+		if(!supportsArithmetic()) {
+			throw new UnsupportedOperationException(getUnitName()+ "category does not support "+ operation );
+		}
+	}
 }
